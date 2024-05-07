b41c486dd9b409e83acd1ae609e67378d989da87
ADONIX\adoni <adonix56@gmail.com>
2024-05-07 09:25:55 -0700

    Created Unreal Top Down Starter Project

---
+++ Source/SunsetHollow/SunsetHollow.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SunsetHollow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SunsetHollow, "SunsetHollow" );
+
+DEFINE_LOG_CATEGORY(LogSunsetHollow)
+ 
