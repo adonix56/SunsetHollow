602293bdfe7ac37a07f37944bb59d335a89c4c7e
ADONIX\adoni <adonix56@gmail.com>
2024-05-09 12:47:24 -0700

    Enabled Gameplay Ability System

--- Source/SunsetHollow/SunsetHollow.Build.cs
+++ Source/SunsetHollow/SunsetHollow.Build.cs
@@ -9,5 +9,7 @@ public class SunsetHollow : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
-    }
+    
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+	}
 }
