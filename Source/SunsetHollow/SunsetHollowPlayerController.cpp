6c11f48394508205b40c972f4194b7d0e4d7b5ad
ADONiX <justin.salanga@gmail.com>
2024-06-07 17:33:33 -0700

    Spinsaw can now damage enemies

--- Source/SunsetHollow/SunsetHollowPlayerController.cpp
+++ Source/SunsetHollow/SunsetHollowPlayerController.cpp
@@ -126,6 +126,7 @@ void ASunsetHollowPlayerController::ActivateAbilityByIndex(int AbilityIndex, boo
 		if (SunsetCharacter) {
 			if (!SetIsAttacking || (SetIsAttacking && !SunsetCharacter->bIsAttacking)) {
 				UAbilitySystemComponent* GASComponent = SunsetCharacter->GetAbilitySystemComponent();
+				GASComponent->CancelAllAbilities();
 				if (GASComponent->TryActivateAbilityByClass(GameplayAbilityArray[AbilityIndex].GameplayAbility)) {
 					StopMovement();
 					SunsetCharacter->bIsAttacking = SetIsAttacking;
