8050e1889773277f711538275067612b50d735b3
Justin Salanga <justin.salanga@gmail.com>
2024-07-02 14:07:42 -0700

    Refactored Damage Calculation. Enemies can now get hit by all Abilities

--- Source/SunsetHollow/GAS/QuickCastAbility.h
+++ Source/SunsetHollow/GAS/QuickCastAbility.h
@@ -27,10 +27,10 @@ protected:
 	void TurnCharacterTowardsCursor(FVector& OutLocation, FVector& OutDirection);
 
 	UFUNCTION(BlueprintCallable)
-	void DamageEnemy(AActor* TargetEnemy, DamageAppliedType DamageType, float MoveDistance = 0.0f, FVector Direction = FVector::ZeroVector, bool FacePlayer = true);
+	void DamageEnemy(AActor* TargetEnemy, DamageAppliedType DamageType, float Damage, float MoveDistance = 0.0f, FVector Direction = FVector::ZeroVector, bool FacePlayer = true);
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Damage, meta = (AllowPrivateAccess = "true"))
-	float Damage;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Damage, meta = (AllowPrivateAccess = "true"))
+	//float Damage;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Damage, meta = (AllowPrivateAccess = "true"))
 	TSet<AEnemyCharacter*> HitEnemies;
